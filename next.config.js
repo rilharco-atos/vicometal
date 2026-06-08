8e3d45a00d650401f12ea7ce9406c4ad857a7212
a932542 <rui.ilharco_ext@a-to-be.com>
2026-06-08 10:42:47 +0100

    fix: add basePath for GitHub Pages subpath deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/vicometal',
+  assetPrefix: '/vicometal/',
   images: {
     unoptimized: true,
   },
